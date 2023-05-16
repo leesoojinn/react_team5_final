3bd1bb0cae6ae6b35fc6afa1f9f2cfffa7e7248a
Kim Hwanhoon <kimhwanhoon@outlook.com>
2023-05-16 14:56:18 +0900

    20230516

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //요소 등록
+const nav = document.querySelector('nav'); //nav 요소 등록
 const modal = document.querySelector('modal1'); // 모달1 요소 등록
 const backdrop = document.querySelector('.backdrop'); // 백드롭 요소 선택
 const write = document.getElementById('write'); //작성하기 버튼 등록
@@ -6,15 +7,30 @@ const modal2 = document.querySelector('modal2'); //모달2 요소 등록
 const modal2Send = document.getElementById('modal2Send'); //모달2 보내기 요소 등록
 const modal2Close = document.getElementById('modal2Close'); //모달2 닫기 요소 등록
 const moreBtn = document.querySelectorAll('.moreBtn'); //people에 more버튼들 요소 등록
+const footer = document.querySelector('footer'); //footer 요소 등록
 //
-
+// smooth scroll
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+// nav창 및 로고 드래그 불가능하게 하기
+nav.addEventListener('mousedown', function (event) {
+    event.preventDefault();
+});
+footer.addEventListener('mousedown', function (event) {
+    event.preventDefault();
+});
 // 로딩 화면 표시 함수
 function showLoading() {
     document.getElementById('loading').style.display = 'flex';
 }
 // 로딩 화면 숨김 함수
+// 로딩화면 일부러 1초 보이기
 function hideLoading() {
-    document.getElementById('loading').style.display = 'none';
+    setTimeout(() => {
+        document.getElementById('loading').style.display = 'none';
+    }, 1000);
     modal.classList.toggle('hide');
     modal2.classList.toggle('hide');
 }
